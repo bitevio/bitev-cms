2b952647cdf69c63468a6ed8da27c935f735bca0
bitevio <110330048+bitevio@users.noreply.github.com>
2022-08-26 10:19:31 +0000

    push

---
+++ providers/ModelProvider.js
@@ -0,0 +1,11 @@
+
+module.exports={
+    models:[
+        {
+            name:"users", // Model name in prisma
+            privateFields:[""] // Array of private field to Hide 
+        },
+       
+    ]
+}
+
